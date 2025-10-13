dbc9225f9161d452e26b8258bfe897a37c621e32
ASC06-CHARISHMA-GIT-REPO <charishmam2118@gmail.com>
2025-10-13 17:06:18 +0530

    lab5 is completed

---
+++ oct2025/src/test/java/Pack1/TC001.java
@@ -0,0 +1,39 @@
+package Pack1;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class TC001 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		WebDriverManager.chromedriver().setup();
+		WebDriver driver=new ChromeDriver();
+		driver.get("https://www.google.com/");
+		System.out.println("The title is:"+driver.getTitle());
+		System.out.println("The url is:"+driver.getCurrentUrl());
+		System.out.println("The page source is:"+driver.getPageSource());
+		WebElement search=driver.findElement(By.id("APjFqb"));
+		search.sendKeys("Software Testing Tools");
+		search.submit();
+	driver.navigate().to("https://www.amazon.in/");
+	System.out.println("The title is:"+driver.getTitle());
+	System.out.println("The url is:"+driver.getCurrentUrl());
+	driver.navigate().back();
+	System.out.println("The title is:"+driver.getTitle());
+	System.out.println("The url is:"+driver.getCurrentUrl());
+	driver.navigate().forward();
+	System.out.println("The title is:"+driver.getTitle());
+	System.out.println("The url is:"+driver.getCurrentUrl());
+		//driver.findElement(By.name("btnI")).click();
+		
+		
+		
+
+	}
+
+}
