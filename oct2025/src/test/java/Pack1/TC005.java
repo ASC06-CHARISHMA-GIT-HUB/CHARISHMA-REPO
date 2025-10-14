a2ac4244241c0c064581f26de5158d50a37af923
ASC06-CHARISHMA-GIT-REPO <charishmam2118@gmail.com>
2025-10-14 10:43:34 +0530

    completed

---
+++ oct2025/src/test/java/Pack1/TC005.java
@@ -0,0 +1,19 @@
+package Pack1;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class TC005 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		WebDriverManager.chromedriver().setup();
+		WebDriver driver=new ChromeDriver();
+		driver.get("https://letcode.in/window");
+		driver.findElement(By.id("multi")).click();
+	}
+
+}
